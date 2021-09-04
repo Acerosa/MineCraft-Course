57d3456fe8db985cbcd3b3c7141fcf1267abec5b
Ricardo Rosa <rjrosa@outlook.com>
2021-09-05 00:19:59 +0100

    Committing a tool set.

--- src/main/java/com/rr/mccourse/item/ModItems.java
+++ src/main/java/com/rr/mccourse/item/ModItems.java
@@ -3,9 +3,9 @@ package com.rr.mccourse.item;
 import com.rr.mccourse.RRMCCourseMod;
 import com.rr.mccourse.util.Registration;
 import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 
 import java.util.function.Supplier;
@@ -26,5 +26,101 @@ public class ModItems {
             Registration.ITEMS.register("coppered_apple",
                     () -> new CopperedApple());
 
+
+    public static final RegistryObject<Item> COPPER_SHOVEL =
+            Registration.ITEMS.register("copper_shovel",
+                    () -> new ShovelItem(ModItemTier.COPPER, 0f, 0f,
+                            new Item.Properties().defaultDurability(150)
+                                    .addToolType(ToolType.SHOVEL, 2)
+                                    .tab(RRMCCourseMod.RRMcCOURSE_TAB)));
+
+
+    public static final RegistryObject<Item> COPPER_SWORD =
+            Registration.ITEMS.register("copper_sword",
+                    () -> new SwordItem(ModItemTier.COPPER, 2, 0.5f,
+                            new Item.Properties()
+                                    .defaultDurability(150)
+                                    .tab(RRMCCourseMod.RRMcCOURSE_TAB)));
+
+
+    public static final RegistryObject<Item> COPPER_PICKAXE =
+            Registration.ITEMS.register("copper_pickaxe",
+                    () -> new PickaxeItem(ModItemTier.COPPER, 0, 0f,
+                            new Item.Properties()
+                                    .defaultDurability(150)
+                                    .addToolType(ToolType.PICKAXE, 1)
+                                    .tab(RRMCCourseMod.RRMcCOURSE_TAB)));
+
+
+    public static final RegistryObject<Item> COPPER_HOE =
+            Registration.ITEMS.register("copper_hoe",
+                    () -> new HoeItem(ModItemTier.COPPER, 0, 0f,
+                            new Item.Properties()
+                                    .defaultDurability(150)
+                                    .addToolType(ToolType.HOE, 1)
+                                    .tab(RRMCCourseMod.RRMcCOURSE_TAB)));
+
+
+    public static final RegistryObject<Item> COPPER_AXE =
+            Registration.ITEMS.register("copper_axe",
+                    () -> new AxeItem(ModItemTier.COPPER, 2.5f, -2.5f,
+                            new Item.Properties()
+                                    .defaultDurability(150)
+                                    .addToolType(ToolType.AXE, 1)
+                                    .tab(RRMCCourseMod.RRMcCOURSE_TAB)));
+
     public static void register() { }
+
+    public enum ModItemTier implements IItemTier
+    {
+        COPPER(2, 150, 2.5f, 0f, 15,
+                Ingredient.of (new ItemStack(ModItems.COPPER_INGOT.get())));
+
+        private final int harvestLevel;
+        private final int maxUses;
+        private final float efficiency;
+        private final float attackDamage;
+        private final int enchantability;
+        private final Ingredient repairMaterial;
+
+        ModItemTier(int harvestLevel, int maxUses, float efficiency,
+                    float attackDamage, int enchantability, Ingredient repairMaterial) {
+            this.harvestLevel = harvestLevel;
+            this.maxUses = maxUses;
+            this.efficiency = efficiency;
+            this.attackDamage = attackDamage;
+            this.enchantability = enchantability;
+            this.repairMaterial = repairMaterial;
+        }
+
+        @Override
+        public int getUses() {
+            return maxUses;
+        }
+
+        @Override
+        public float getSpeed() {
+            return efficiency;
+        }
+
+        @Override
+        public float getAttackDamageBonus() {
+            return attackDamage;
+        }
+
+        @Override
+        public int getLevel() {
+            return harvestLevel;
+        }
+
+        @Override
+        public int getEnchantmentValue() {
+            return enchantability;
+        }
+
+        @Override
+        public Ingredient getRepairIngredient() {
+            return repairMaterial;
+        }
+    }
 }
